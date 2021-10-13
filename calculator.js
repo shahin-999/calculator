cb4a6cb0cd59cca20d285d80a7fba62a36ac1375
Md Shahin Shanaous <52136857+shahin1998@users.noreply.github.com>
2021-10-13 13:13:43 +0600

    fix bugs(backspace, add history lavel and remove operator duplication)

--- calculator.js
+++ calculator.js
@@ -1,13 +1,15 @@
 var screen = document.getElementById('txt-fild');
+var historyScreen = document.getElementById('history');
 var button = document.querySelectorAll('button');
 
-var allValue = '';
-
+var allValue = '' , historyValue='';
 for (item of button){
-    item.addEventListener('click', (value)=>{
-        var buttonText = value.target.innerText;
-        console.log(buttonText);
-
+    item.addEventListener('click', (v)=>{
+        var buttonText = v.target.innerText;
+        let temp = allValue[allValue.length-1]
+        if( (temp == '+' || temp == '-' || temp == '*' || temp == '/') && (buttonText == '+' || buttonText == '-' || buttonText == '×' || buttonText == '÷') ){
+            allValue = allValue.substring(0,allValue.length-1);
+        }
         if(buttonText == '×'){
             buttonText = '*';
             allValue += buttonText;
@@ -22,17 +24,21 @@ for (item of button){
 
         else if(buttonText == 'C'){
             allValue='';
+            historyValue = '';
             screen.value=allValue;
+            historyScreen.innerText =historyValue;
         }
 
         else if(buttonText == '←'){
-            allValue=allValue(0,-1);
+            allValue=allValue.substring(0,allValue.length-1);
             screen.value=allValue;
         }
 
         else if(buttonText == '='){
+            historyValue = allValue;
             allValue = eval(allValue);
             screen.value = allValue;
+            historyScreen.innerHTML = historyValue;
             if (allValue == '' || allValue == '0'){
                 allValue='';
             }
